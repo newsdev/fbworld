5c73acd1572dae67a2ad930901c1a3d44a63935a
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-11-20 17:29:03 -0500

    Added environment variables

--- fbworld/write_kubes.py
+++ fbworld/write_kubes.py
@@ -1,10 +1,21 @@
 import json
+import sys
 
-n = 2
+txts = sys.argv[1:]
+n = len(txts)
+
 script = {}
 script["apiVersion"] = "v1"
 script["kind"] = "List"
 items = []
+
+"""Download all txts into list"""
+list_lists = []
+for txt in txts:
+    with open(txt, 'r') as f: 
+       list_lists.append([line.strip() for line in f]) 
+
+"""Set up script"""
 for i in range(n):
 
     pod = {}
@@ -18,11 +29,13 @@ for i in range(n):
     container["name"] = "container-{}".format(str(i))
     container["image"] = "duckduckgo"
     container["command"] = ["scrape-facebook/try-docker-electron/docker-compose", "run", "electron", "bash", "|", "node", "scrape-facebook/try-docker-electron/index.js"]
+    container["environment"] = ",".join(list_lists[i])
     #container["command"] = ["python", "fbworld/fbworld/get_member_dict.py", "{}.txt".format(str(i))]
 
     pod["spec"]["containers"].append(container)
     items.append(pod) 
 
+"""Write script to json"""
 script["items"] = items
 with open("fbtest.json", "w") as f:
     json.dump(script, f, indent=4)
