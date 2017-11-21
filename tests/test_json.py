f6e2de076f80ad0a64ad48b3ecb2be245b06e561
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-11-21 10:41:37 -0500

    changed shape of unit test

--- tests/test_json.py
+++ tests/test_json.py
@@ -9,6 +9,9 @@ class GetMembersDictTestCase(unittest.TestCase):
     data_path = "%s/member_groups_output.json" % DATA_DIR.replace('tests/', 'data')
 
     results = None
+    key_0 = "391246377893952"
+    key_1 = "1136709433084412"
+    key_2 = "280768775366358"
 
     def setUp(self, **kwargs):
         """
@@ -24,21 +27,21 @@ class GetMembersDictTestCase(unittest.TestCase):
             self.results = dict(json.loads(readfile.read()))
 
     def test_first_two_groups(self):
-        group_0 = set(self.results['data'][0]['members'])
-        group_1 = set(self.results['data'][1]['members'])
+        group_0 = set(self.results['data'].get(key_0)['members'])
+        group_1 = set(self.results['data'].get(key_1)['members'])
 
         self.assertFalse(group_0.union(group_1) == group_1)
 
     def test_second_two_groups(self):
         pass
-        # group_1 = set(self.results['data'][1]['members'])
-        # group_2 = set(self.results['data'][2]['members'])
+        # group_1 = set(self.results['data'].get(key_1)['members'])
+        # group_2 = set(self.results['data'].get(key_2)['members'])
             
         # self.assertFalse(group_1.union(group_2) == group_2)
     
     def test_outer_two_groups(self):
         pass
-        # group_0 = set(self.results['data'][0]['members'])
-        # group_2 = set(self.results['data'][2]['members'])
+        # group_0 = set(self.results['data'].get(key_0)['members'])
+        # group_2 = set(self.results['data'].get(key_2)['members'])
             
         # self.assertFalse(group_0.union(group_2) == group_2)
