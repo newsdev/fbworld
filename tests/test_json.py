18a5247ae3b41f55a82b3608ea7b2ff10f382162
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-11-21 10:29:32 -0500

    Adds a working test. Make sure to add2virtualenv tests and pip install -r requirements.txt

---
+++ tests/test_json.py
@@ -0,0 +1,44 @@
+import json
+import os
+import unittest
+
+
+class GetMembersDictTestCase(unittest.TestCase):
+    DATA_DIR = "%s/" % os.path.dirname(os.path.realpath(__file__))
+
+    data_path = "%s/member_groups_output.json" % DATA_DIR.replace('tests/', 'data')
+
+    results = None
+
+    def setUp(self, **kwargs):
+        """
+        This works now. But the data shape is wrong for the tests you have below.
+        self.results['data'][0] will make an error because there's no [0] in a dict.
+
+        Traceback (most recent call last):
+            File "/Users/jbowers/src/nyt/fbworld/tests/test_json.py", line 22, in test_first_two_groups
+                group_0 = set(self.results['data'][0]['members'])
+        KeyError: 0
+        """
+        with open(self.data_path, 'r') as readfile:
+            self.results = dict(json.loads(readfile.read()))
+
+    def test_first_two_groups(self):
+        group_0 = set(self.results['data'][0]['members'])
+        group_1 = set(self.results['data'][1]['members'])
+
+        self.assertFalse(group_0.union(group_1) == group_1)
+
+    def test_second_two_groups(self):
+        pass
+        # group_1 = set(self.results['data'][1]['members'])
+        # group_2 = set(self.results['data'][2]['members'])
+            
+        # self.assertFalse(group_1.union(group_2) == group_2)
+    
+    def test_outer_two_groups(self):
+        pass
+        # group_0 = set(self.results['data'][0]['members'])
+        # group_2 = set(self.results['data'][2]['members'])
+            
+        # self.assertFalse(group_0.union(group_2) == group_2)
