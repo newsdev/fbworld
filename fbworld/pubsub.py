b507f46ba9d10d52f66e7f276dd7de6af8b4e421
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-11-29 15:07:27 -0500

    adding pubsbu

---
+++ fbworld/pubsub.py
@@ -0,0 +1,32 @@
+import time
+from google.cloud import pubsub_v1
+
+"""Publisher"""
+def publish_messages(project, topic_name):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project, topic_name)
+
+    for n in range(1, 10):
+        data = u'Message number {}'.format(n)
+        data = data.encode('utf-8')
+        publisher.publish(topic_path, data=data)
+
+    print('Published messages')
+
+"""Subscriber"""
+def receive_messages(project, subscription_name):
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(project, subscription_name)
+    
+    def callback(message):
+        print('Received message: {}'.format(message))
+        message.ack()
+
+    subscriber.subscribe(subscription_path, callback=callback)
+    print('Listening for messages on {}'.format(subscription_path))
+    while True:
+        time.sleep(60)
+
+if __name__ == '__main__':
+    receive_messages("nytint-stg", "fbworld")
+    publish_messages("nytint-stg", "fbworld")
