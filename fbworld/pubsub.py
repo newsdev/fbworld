37798f88d94775aef2a03e3718c391054d005df3
Lilia Chang <liliachang@liliachang-wash.ent.nytint.com>
2017-12-01 14:02:30 -0500

    test pubsub

--- fbworld/pubsub.py
+++ fbworld/pubsub.py
@@ -6,10 +6,14 @@ def publish_messages(project, topic_name):
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project, topic_name)
 
-    for n in range(1, 10):
+    data = u'Message number {}'.format("10215028733130789")
+    data = data.encode('utf-8')
+    publisher.publish(topic_path, data=data)
+
+    """for n in range(1, 10):
         data = u'Message number {}'.format(n)
         data = data.encode('utf-8')
-        publisher.publish(topic_path, data=data)
+        publisher.publish(topic_path, data=data)"""
 
     print('Published messages')
 
